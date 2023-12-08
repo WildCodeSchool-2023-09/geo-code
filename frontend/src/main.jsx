0cfcbdbc0f00d26b71f8507b5d24ddd471739a94
Raphael Foulon-Binet <raphael.foulonbinet@gmail.com>
2023-12-08 11:00:54 +0100

    Ajout du Panel Admin +  Fix title dans la navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import ErrorPage from "./pages/404";
+import AdminPanel from "./pages/AdminPanel";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/admin",
+        element: <AdminPanel />,
       },
       {
         path: "*",
