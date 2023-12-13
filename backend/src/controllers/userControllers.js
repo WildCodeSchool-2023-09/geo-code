e120eb0b2ef3286e61a5c38a890c69454d3b7371
Raphael Foulon-Binet <raphael.foulonbinet@gmail.com>
2023-12-13 17:14:16 +0100

    Creation userManager + userControllers pour afficher la liste des users avec l'adresse http://localhost:3310/api/users

---
+++ backend/src/controllers/userControllers.js
@@ -0,0 +1,59 @@
+// Import access to database tables
+const tables = require("../tables");
+
+// The B of BREAD - Browse (Read All) operation
+const browse = async (req, res, next) => {
+  try {
+    const users = await tables.users.readAll();
+    res.json(users);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// The R of BREAD - Read operation
+const read = async (req, res, next) => {
+  try {
+    const user = await tables.users.read(req.params.id);
+
+    if (user == null) {
+      res.sendStatus(404);
+    } else {
+      res.json(user);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+// The E of BREAD - Edit (Update) operation
+// This operation is not yet implemented
+
+// The A of BREAD - Add (Create) operation
+const add = async (req, res, next) => {
+  // Extract the item data from the request body
+  const user = req.body;
+
+  try {
+    // Insert the item into the database
+    const insertId = await tables.users.create(user);
+
+    // Respond with HTTP 201 (Created) and the ID of the newly inserted item
+    res.status(201).json({ insertId });
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+// The D of BREAD - Destroy (Delete) operation
+// This operation is not yet implemented
+
+// Ready to export the controller functions
+module.exports = {
+  browse,
+  read,
+  // edit,
+  add,
+  // destroy,
+};
