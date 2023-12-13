5a3aba4086e53e19e33c8ff5d0526ade1e11e356
Raphael Foulon-Binet <143555358+Rapha2202@users.noreply.github.com>
2023-12-13 09:13:57 +0100

    Fix main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
         path: "/reservations",
         element: <Reservation />,
       },
-
+      {
         path: "/ajout-bornes",
         element: <AdminAddBornes />,
       },
