acf6ee9b573d0bbe37e0f883d7ee23a17a6441ef
batsave <batsave.twitch@gmail.com>
2023-12-15 16:32:38 +0100

    👌 IMPROVE: Refactor Contact page and update styling

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -3,11 +3,14 @@ import emailjs from "@emailjs/browser";
 
 import "../scss/contact.scss";
 
-export default async function Contact() {
+export default function Contact() {
   const [details, setDetails] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    category: "",
     subject: "",
     message: "",
-    to_email: "",
   });
 
   const handleDetailsChange = (event) => {
@@ -28,7 +31,10 @@ export default async function Contact() {
         import.meta.env.VITE_EMAIL_SERVICE_ID,
         import.meta.env.VITE_EMAIL_TEMPLATE_ID,
         {
-          to_email: details.to_email,
+          firstname: details.firstname,
+          lastname: details.lastname,
+          category: details.category,
+          email: details.email,
           subject: details.subject,
           message: details.message,
         }
@@ -42,43 +48,101 @@ export default async function Contact() {
   };
 
   return (
-    <div className="main">
-      <div>
-        <span>To :</span>
-        <input
-          name="to_email"
-          value={details.to_email}
-          onChange={handleDetailsChange}
-          type="email"
-          placeholder="Required .. "
-        />
+    <main className="backgroundImageMain">
+      <div className="from_contact_container">
+        <div className="form_placeholder">
+          <p className="form_placeholder_title">Nom</p>
+          <input
+            className="form_placeholder_input"
+            name="lastname"
+            value={details.lastname}
+            onChange={handleDetailsChange}
+            type="text"
+            placeholder="Doe"
+            required
+          />
+        </div>
+        <div className="form_placeholder">
+          <p className="form_placeholder_title">Prénom</p>
+          <input
+            className="form_placeholder_input"
+            name="firstname"
+            value={details.firstname}
+            onChange={handleDetailsChange}
+            type="text"
+            placeholder="John"
+            required
+          />
+        </div>
+        <div className="form_placeholder">
+          <p className="form_placeholder_title">Email</p>
+          <input
+            className="form_placeholder_input"
+            name="email"
+            value={details.email}
+            onChange={handleDetailsChange}
+            type="email"
+            placeholder="john_doe@exemple.com"
+            required
+          />
+        </div>
+        <div className="form_placeholder">
+          <label htmlFor="category-selector" className="form_placeholder_title">
+            Catégorie
+          </label>
+          <select
+            id="category-selector"
+            className="form_placeholder_subject"
+            name="category"
+            onChange={handleDetailsChange}
+            required
+          >
+            <option value="">-</option>
+            <option value="Informations">Demande d'informations</option>
+            <option value="Partenariat">Demande de partenariat</option>
+            <option value="Assistance">Demande d'assistance</option>
+            <option value="Autres">Autres</option>
+          </select>
+        </div>
+        <div className="form_placeholder">
+          <p className="form_placeholder_title">Sujet</p>
+          <input
+            className="form_placeholder_input"
+            name="subject"
+            value={details.subject}
+            onChange={handleDetailsChange}
+            type="text"
+            placeholder="Demande de ..."
+            required
+          />
+        </div>
+        <div className="form_placeholder">
+          <p className="form_placeholder_title">Message</p>
+          <textarea
+            className="form_placeholder_input form_placeholder_textarea"
+            name="message"
+            value={details.message}
+            onChange={handleDetailsChange}
+            type="text"
+            placeholder="Votre message ici ..."
+            required
+          />
+        </div>
+        <button
+          type="button"
+          disabled={
+            !details.firstname ||
+            !details.lastname ||
+            !details.subject ||
+            !details.email ||
+            !details.category ||
+            !details.message
+          }
+          onClick={handleSendEmail}
+        >
+          <span>Envoyer le Message</span>
+        </button>
       </div>
-      <div>
-        <span>Subject :</span>
-        <input
-          name="subject"
-          value={details.subject}
-          onChange={handleDetailsChange}
-          type="text"
-          placeholder="Required"
-        />
-      </div>
-      <div>
-        <span>Message</span>
-        <textarea
-          name="message"
-          value={details.message}
-          onChange={handleDetailsChange}
-          placeholder="Required .. "
-        />
-      </div>
-      <button
-        type="button"
-        disabled={!details.to_email || !details.subject || !details.message}
-        onClick={handleSendEmail}
-      >
-        <span>Send Email</span>
-      </button>
-    </div>
+    </main>
   );
 }
