70599299f0a674770d78bec6b6e54bfae8d1d5b2
Morgane Debarge <morganedebarge.05@gmail.com>
2023-12-19 08:39:39 +0100

    filtre fonctionnel sur la page map

--- frontend/src/components/BornesMarker.jsx
+++ frontend/src/components/BornesMarker.jsx
@@ -1,5 +1,7 @@
 import { Marker } from "react-leaflet";
 import L from "leaflet";
+import { useContext } from "react";
+import FilterContext from "../Context/ResearchContext";
 import iconMarker from "../assets/borne-marker-black.svg";
 
 function GetIcon(_iconSize) {
@@ -10,18 +12,72 @@ function GetIcon(_iconSize) {
 }
 
 function BornesMarker() {
+  const { research } = useContext(FilterContext);
   const bornes = [
-    { name: "Paris", lat: 48.852969, lng: 2.349903 },
-    { name: "Brest", lat: 48.383, lng: -4.5 },
-    { name: "Quimper", lat: 48.0, lng: -4.1 },
-    { name: "Bayonne", lat: 43.5, lng: -1.467 },
+    {
+      names: "Paris",
+      lat: 48.852969,
+      lng: 2.349903,
+      code: "75000",
+      enseigne: "Une",
+      puissance: "1250",
+      prise: "Type 2",
+      tarification: "payant",
+      disponible: "false",
+      pdc: 1,
+    },
+    {
+      names: "Brest",
+      lat: 48.383,
+      lng: -4.5,
+      code: "27000",
+      puissance: "1200",
+      enseigne: "Deux",
+      prise: "Type 2",
+      tarification: "payant",
+      disponible: "false",
+      pdc: 1,
+    },
+    {
+      names: "Quimper",
+      lat: 48.0,
+      lng: -4.1,
+      code: "22000",
+      puissance: "1050",
+      enseigne: "Une",
+      prise: "Type 2",
+      tarification: "payant",
+      disponible: "false",
+      pdc: 1,
+    },
+    {
+      names: "Bayonne",
+      lat: 43.5,
+      lng: -1.467,
+      code: "44000",
+      puissance: "1220",
+      enseigne: "Deux",
+      prise: "Type 1",
+      tarification: "gratuit",
+      disponible: "true",
+      pdc: 12,
+    },
   ];
 
   return (
     <div>
-      {bornes.map((borne) => (
-        <Marker position={borne} key={borne.name} icon={GetIcon()} />
-      ))}
+      {bornes
+        .filter(
+          (borne) =>
+            borne.code.includes(research.code) &&
+            borne.enseigne.includes(research.enseigne) &&
+            borne.tarification.includes(research.tarification) &&
+            borne.puissance.includes(research.puissance) &&
+            borne.prise.includes(research.prise)
+        )
+        .map((borne) => (
+          <Marker position={borne} key={borne.name} icon={GetIcon()} />
+        ))}
     </div>
   );
 }
