3352d1597ab994b341087e8f8a9fafee015173b2
Jeremy Illien <mr.illien.jeremy@gmail.com>
2023-12-20 17:04:33 +0100

    add env file

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import "../scss/register.scss";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Register() {
   const [user, setUser] = useState({
-    name: "",
+    lastname: "",
+    firstname: "",
     email: "",
     password: "",
     confirm: "",
@@ -24,176 +26,242 @@ function Register() {
   };
 
   return (
-    <div className="wrapper">
+    <main className="register-page">
       <Link to="/register" />
       <h1>Créer un compte</h1>
-      <p>informations personnelles</p>
+      <h2> informations personnelles </h2>
       <form className="userInfo">
-        <div>
-          <label htmlFor="lastname">Nom</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="lastname">
+            Nom
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.lastname}
             type="text"
             name="lastname"
             id="lastname"
             onChange={handleChange}
+            placeholder="Carpenter"
           />
         </div>
-        <div>
-          <label htmlFor="firstname">Prénom</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="firstname">
+            Prénom
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.firstname}
             type="text"
             name="firstname"
             id="firstname"
             onChange={handleChange}
+            placeholder="John"
           />
         </div>
-        <div>
-          <label htmlFor="email">Email</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="email">
+            Email
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.email}
             type="email"
             name="email"
             id="email"
             onChange={handleChange}
+            placeholder="example@gmail.com"
           />
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="password">
+            Password
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.password}
             type="password"
             name="password"
             id="password"
             onChange={handleChange}
+            placeholder="Enter your password"
           />
         </div>
-        <div>
-          <label htmlFor="confirm">Confirm</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="confirm">
+            Confirm
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.confirm}
             type="password"
             name="confirm"
             id="confirm"
             onChange={handleChange}
+            placeholder="Confirm your password"
           />
         </div>
-        <div>
-          <label htmlFor="anniversary">Anniversary</label>
+        <div className="content_input">
+          <label className="content_input_label_space" htmlFor="anniversary">
+            Anniversaire
+          </label>
           <input
+            className="content_input_placeholder_space"
             value={user.anniversary}
             type="text"
             name="anniversary"
             id="anniversary"
             onChange={handleChange}
+            placeholder="25/12/2023"
           />
         </div>
-        <div>
-          <label htmlFor="adress">Adresse</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="adress">
+            Adresse
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.adress}
             type="text"
             name="adress"
             id="adress"
             onChange={handleChange}
+            placeholder="Votre adresse"
           />
         </div>
-        <div>
-          <label htmlFor="postalcode">Code postal</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="postalcode">
+            Code postal
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.postalcode}
             type="text"
             name="postalcode"
             id="postalcode"
             onChange={handleChange}
+            placeholder="00000"
           />
         </div>
-        <div>
-          <label htmlFor="city">Ville</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="city">
+            Ville
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.city}
             type="text"
             name="city"
             id="city"
             onChange={handleChange}
+            placeholder="New York"
           />
         </div>
-        <div>
-          <label htmlFor="phone">Téléphone</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="phone">
+            Téléphone
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.phone}
             type="text"
             name="phone"
             id="phone"
             onChange={handleChange}
+            placeholder="+33 00 00 00 00 00"
           />
         </div>
-        <div>
-          <label htmlFor="vehicule">Véhicule</label>
+        <div className="content_input">
+          <label className="content_input_label_space" htmlFor="vehicule">
+            Véhicule(s)
+          </label>
           <input
+            className="content_input_placeholder_space"
             value={user.vehicule}
             type="text"
             name="vehicule"
             id="vehicule"
             onChange={handleChange}
+            placeholder="Nombre de véhicules"
           />
         </div>
       </form>
-      <p>informations sur le(s) véhicules</p>
+      <h3>informations sur le(s) véhicules</h3>
       <form className="vehiculeInfo">
-        <div>
-          <label htmlFor="marque">Marque</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="marque">
+            Marque
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.marque}
             type="text"
             name="marque"
             id="marque"
             onChange={handleChange}
+            placeholder="Renault"
           />
         </div>
-        <div>
-          <label htmlFor="model">Modèle</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="model">
+            Modèle
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.model}
             type="text"
             name="model"
             id="model"
             onChange={handleChange}
+            placeholder="Megane RS"
           />
         </div>
-        <div>
-          <label htmlFor="prise">Prise</label>
+        <div className="content_input">
+          <label className="content_input_label" htmlFor="prise">
+            Prise
+          </label>
           <input
+            className="content_input_placeholder"
             value={user.prise}
             type="text"
             name="prise"
             id="prise"
             onChange={handleChange}
+            placeholder="Type A"
           />
         </div>
-        <div>
-          <input type="checkbox" id="cgu-toggle" />
-          <label htmlFor="cgu-toggle" className="register-button">
-            J'accepte les conditions générales d'utilisations
+      </form>
+      <div className="toggle-button">
+        <div className="button_dispose">
+          <label className="switch_button">
+            <input type="checkbox" />
+            <span className="slider round" />
           </label>
+          <p>J'accepte les conditions générales d'utilisations</p>
         </div>
-        <div>
-          <input type="checkbox" id="conf-toggle" />
-          <label htmlFor="conf-toggle" className="register-button">
-            J'accepte les politiques de confidentialité
+        <div className="button_dispose">
+          <label className="switch_button">
+            <input type="checkbox" />
+            <span className="slider round" />
           </label>
+          <p>J'accepte les politiques de confidentialité</p>
         </div>
+      </div>
+      <div className="button-form">
         <div>
-          <button type="submit">Créer mon compte</button>
+          <button className="create_button" type="submit">
+            Créer mon compte
+          </button>
         </div>
         <div>
           <Link to="/">
-            <button type="button">Annuler</button>
+            <button className="cancel_button" type="button">
+              Annuler
+            </button>
           </Link>
         </div>
-      </form>
-    </div>
+      </div>
+    </main>
   );
 }
 
