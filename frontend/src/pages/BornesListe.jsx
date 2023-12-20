e6c9a222aca60c950146ac0cb9ad9f0c5f50f11b
Morgane Debarge <morganedebarge.05@gmail.com>
2023-12-20 16:01:46 +0100

    filtre fonctionnel de partout. Youpi!

--- frontend/src/pages/BornesListe.jsx
+++ frontend/src/pages/BornesListe.jsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react";
+import convertToDistance from "../services/ConvertToDistance";
 import FilterContext from "../Context/ResearchContext";
 import BorneCardUser from "../components/BorneCardUser";
 import LocationContext from "../Context/locationContext";
@@ -14,27 +15,6 @@ function BornesListe() {
   }, [research]);
 
   const { position } = useContext(LocationContext);
-
-  const convertToDistance = (lat, lng, poslat, poslng) => {
-    const radiusEarthkm = 6371.07103;
-    const radlat = lat * (Math.PI / 180);
-    const radPositionLat = poslat * (Math.PI / 180);
-    const radlatDiff = (lat - poslat) * (Math.PI / 180);
-    const radlngDiff = (lng - poslng) * (Math.PI / 180);
-    const distance =
-      2 *
-      radiusEarthkm *
-      Math.sin(
-        Math.sqrt(
-          Math.sin(radlatDiff / 2) * Math.sin(radlatDiff / 2) +
-            Math.cos(radPositionLat) *
-              Math.cos(radlat) *
-              Math.sin(radlngDiff / 2) *
-              Math.sin(radlngDiff / 2)
-        )
-      );
-    return distance.toFixed(1);
-  };
 
   return (
     <div className="borneListPage">
---
+++ frontend/src/services/ConvertToDistance.js
@@ -0,0 +1,21 @@
+function convertToDistance(lat, lng, poslat, poslng) {
+  const radiusEarthkm = 6371.07103;
+  const radlat = lat * (Math.PI / 180);
+  const radPositionLat = poslat * (Math.PI / 180);
+  const radlatDiff = (lat - poslat) * (Math.PI / 180);
+  const radlngDiff = (lng - poslng) * (Math.PI / 180);
+  const distance =
+    2 *
+    radiusEarthkm *
+    Math.sin(
+      Math.sqrt(
+        Math.sin(radlatDiff / 2) * Math.sin(radlatDiff / 2) +
+          Math.cos(radPositionLat) *
+            Math.cos(radlat) *
+            Math.sin(radlngDiff / 2) *
+            Math.sin(radlngDiff / 2)
+      )
+    );
+  return distance.toFixed(1);
+}
+export default convertToDistance;
