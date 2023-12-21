f809cb269e15c1461d7a9c296361783a8ccbed60
Morgane Debarge <morganedebarge.05@gmail.com>
2023-12-21 14:46:42 +0100

    correction de schema db pour correspondance avec ce qu'ayoub a validé et retrait des tables accessibilite et enseigne

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,104 +1,84 @@
-create table accessibilite
-(
-    id     int auto_increment
-        primary key,
-    valeur varchar(255) not null
+CREATE TABLE `users` (
+	`id` int NOT NULL,
+	`firstname` varchar(80) NOT NULL,
+	`lastname` varchar(80) NOT NULL,
+	`rue` varchar(255) NOT NULL
+	`code_postal` int NOT NULL,
+	`ville` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`password` varchar(80) NOT NULL,
+	`logged_in` BOOLEAN NOT NULL,
+	`nb_vehicule` int NOT NULL,
+	`is_admin` BOOLEAN NOT NULL,
+	`anniversaire` DATE NOT NULL,
+	`inscription` DATE NOT NULL,
+	`derniere_maj` DATE NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-create table enseigne
-(
-    id   int auto_increment
-        primary key,
-    name varchar(255) not null
+CREATE TABLE `Borne` (
+	`id_station` varchar(255) NOT NULL,
+	`n_station` varchar(255) NOT NULL,
+	`ad_station` varchar(255) NOT NULL,
+	`code_insee` INT NOT NULL,
+	`xlongitude` FLOAT NOT NULL,
+	`ylatitude` FLOAT NOT NULL,
+	`puiss_max` FLOAT NOT NULL,
+	`tarification_id` int NOT NULL,
+	`accessibilite` varchar(80) NOT NULL,
+	`type_prise` varchar(80) NOT NULL,
+	`n_enseigne`  varchar (80) NOT NULL,
+	`date_maj` DATE NOT NULL,
+	PRIMARY KEY (`id_station`)
 );
 
-create table marque
-(
-    id   int auto_increment
-        primary key,
-    name varchar(255) not null
+CREATE TABLE `Marque` (
+	`id` int NOT NULL,
+	`name` varchar(80) NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-create table modele
-(
-    id         int auto_increment
-        primary key,
-    name       varchar(255) not null,
-    marque_id  int          not null,
-    type_prise varchar(255) not null,
-    constraint marque_id
-        foreign key (marque_id) references marque (id)
+CREATE TABLE `Tarification` (
+	`id` int NOT NULL,
+	`valeur` INT NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-create table tarification
-(
-    id     int auto_increment
-        primary key,
-    valeur varchar(255) not null
+CREATE TABLE `Modele` (
+	`id` int NOT NULL,
+	`name` varchar(200) NOT NULL,
+	`marque_id` int NOT NULL,
+	`type_prise`  varchar (150) NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-create table borne
-(
-    id               int auto_increment
-        primary key,
-    name             varchar(255) not null,
-    code_postal      int          not null,
-    lng              float        not null,
-    lat              float        not null,
-    puissance        float        not null,
-    tarification_id  int          not null,
-    accessibilite_id int          not null,
-    booked           tinyint(1)   not null,
-    type_prise       varchar(255) not null,
-    enseigne_id      int          not null,
-    constraint accesibilite_id
-        foreign key (accessibilite_id) references accessibilite (id),
-    constraint enseigne_id
-        foreign key (enseigne_id) references enseigne (id),
-    constraint tarification_id
-        foreign key (tarification_id) references tarification (id)
+CREATE TABLE `vehicule` (
+	`id` int NOT NULL,
+	`prorpietaire_id` int NOT NULL,
+	`modele_id` int NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-create table users
-(
-    id          int auto_increment
-        primary key,
-    firstname   varchar(255)         not null,
-    lastname    varchar(255)         not null,
-    code_postal int                  not null,
-    ville       varchar(255)         not null,
-    email       varchar(255)         not null,
-    password    varchar(255)         not null,
-    logged_in   tinyint(1) default 0 not null,
-    nb_vehicule int                  not null,
-    isAdmin     tinyint(1) default 0 not null,
-    birthday    date                 not null
+CREATE TABLE `Reservation` (
+	`id` int NOT NULL,
+	`borne_id` int NOT NULL,
+	`vehicule_id` int NOT NULL,
+	`date_reservation` DATE NOT NULL,
+	`heure` TIME NOT NULL,
+	`heure_fin` TIME NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
-create table vehicule
-(
-    id              int auto_increment
-        primary key,
-    proprietaire_id int not null,
-    modele_id       int not null,
-    id_type_prise   int not null,
-    constraint modele_id
-        foreign key (modele_id) references modele (id),
-    constraint proprietaire_id
-        foreign key (proprietaire_id) references users (id)
-);
+ALTER TABLE `Borne` ADD CONSTRAINT `Borne_fk0` FOREIGN KEY (`tarification_id`) REFERENCES `Tarification`(`id`);
 
-create table reservation
-(
-    id          int auto_increment
-        primary key,
-    vehicule_id int  not null,
-    borne_id    int  not null,
-    date_book   date not null,
-    heure       int  not null,
-    constraint borne_id
-        foreign key (borne_id) references borne (id),
-    constraint vehicule_id
-        foreign key (vehicule_id) references vehicule (id)
-);
+ALTER TABLE `Modele` ADD CONSTRAINT `Modele_fk0` FOREIGN KEY (`marque_id`) REFERENCES `Marque`(`id`);
+
+ALTER TABLE `vehicule` ADD CONSTRAINT `vehicule_fk0` FOREIGN KEY (`prorpietaire_id`) REFERENCES `users`(`id`);
+
+ALTER TABLE `vehicule` ADD CONSTRAINT `vehicule_fk1` FOREIGN KEY (`modele_id`) REFERENCES `Modele`(`id`);
+
+
+ALTER TABLE `Reservation` ADD CONSTRAINT `Reservation_fk0` FOREIGN KEY (`borne_id`) REFERENCES `Borne`(`id_station`);
+
+ALTER TABLE `Reservation` ADD CONSTRAINT `Reservation_fk1` FOREIGN KEY (`vehicule_id`) REFERENCES `vehicule`(`id`);
 
