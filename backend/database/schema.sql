bcbcab2302edb31350221203d0b1efbdd5300ecf
Morgane Debarge <morganedebarge.05@gmail.com>
2023-12-21 16:45:53 +0100

    essaye import csv

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,7 +1,7 @@
 USE p3;
- DROP TABLE IF EXISTS 'users';
-CREATE TABLE `users` (
-	`id` int NOT NULL,
+ DROP TABLE IF EXISTS 'user';
+CREATE TABLE `user` (
+	id int AUTO_INCREMENT NOT NULL,
 	`firstname` varchar(80) NOT NULL,
 	`lastname` varchar(80) NOT NULL,
 	`rue` varchar(255) NOT NULL
@@ -18,8 +18,8 @@ CREATE TABLE `users` (
 	PRIMARY KEY (`id`)
 );
 
-DROP TABLE IF EXISTS 'Bornes';
-CREATE TABLE `Bornes` (
+DROP TABLE IF EXISTS 'borne';
+CREATE TABLE `borne` (
 	`id_station` varchar(255) NOT NULL,
 	`n_station` varchar(255) NOT NULL,
 	`ad_station` varchar(255) NOT NULL,
@@ -35,40 +35,40 @@ CREATE TABLE `Bornes` (
 	PRIMARY KEY (`id_station`)
 );
 
-DROP TABLE IF EXISTS 'Marque';
-CREATE TABLE `Marque` (
-	`id` int NOT NULL,
+DROP TABLE IF EXISTS 'marque';
+CREATE TABLE `marque` (
+	`id` int AUTO_INCREMENT NOT NULL,
 	`name` varchar(80) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-DROP TABLE IF EXISTS 'Tarification';
-CREATE TABLE `Tarification` (
+DROP TABLE IF EXISTS 'tarification';
+CREATE TABLE `tarification` (
 	`id` int NOT NULL,
 	`valeur` INT NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-DROP TABLE IF EXISTS 'Modele';
-CREATE TABLE `Modele` (
-	`id` int NOT NULL,
+DROP TABLE IF EXISTS 'modele';
+CREATE TABLE `modele` (
+	`id` int AUTO_INCREMENT NOT NULL,
 	`name` varchar(200) NOT NULL,
 	`marque_id` int NOT NULL,
 	`type_prise`  varchar (150) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-DROP TABLE IF EXISTS 'Vehicule';
-CREATE TABLE `Vehicule` (
-	`id` int NOT NULL,
+DROP TABLE IF EXISTS 'vehicule';
+CREATE TABLE `vehicule` (
+	`id` int AUTO_INCREMENT NOT NULL,
 	`prorpietaire_id` int NOT NULL,
 	`modele_id` int NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-DROP TABLE IF EXISTS 'Reservation';
-CREATE TABLE `Reservation` (
-	`id` int NOT NULL,
+DROP TABLE IF EXISTS 'reservation';
+CREATE TABLE `reservation` (
+	`id` int  AUTO_INCREMENT NOT NULL,
 	`borne_id` int NOT NULL,
 	`vehicule_id` int NOT NULL,
 	`date_reservation` DATE NOT NULL,
@@ -77,19 +77,19 @@ CREATE TABLE `Reservation` (
 	PRIMARY KEY (`id`)
 );
 
-ALTER TABLE `Bornes` ADD CONSTRAINT `Borne_fk0` FOREIGN KEY (`tarification_id`) REFERENCES `Tarification`(`id`);
+ALTER TABLE `borne` ADD CONSTRAINT `borne_fk0` FOREIGN KEY (`tarification_id`) REFERENCES `tarification`(`id`);
 
-ALTER TABLE `Modele` ADD CONSTRAINT `Modele_fk0` FOREIGN KEY (`marque_id`) REFERENCES `Marque`(`id`);
+ALTER TABLE `modele` ADD CONSTRAINT `modele_fk0` FOREIGN KEY (`marque_id`) REFERENCES `marque`(`id`);
 
-ALTER TABLE `Vehicule` ADD CONSTRAINT `vehicule_fk0` FOREIGN KEY (`prorpietaire_id`) REFERENCES `users`(`id`);
+ALTER TABLE `vehicule` ADD CONSTRAINT `vehicule_fk0` FOREIGN KEY (`prorpietaire_id`) REFERENCES `user`(`id`);
 
-ALTER TABLE `Vehicule` ADD CONSTRAINT `vehicule_fk1` FOREIGN KEY (`modele_id`) REFERENCES `Modele`(`id`);
+ALTER TABLE `vehicule` ADD CONSTRAINT `vehicule_fk1` FOREIGN KEY (`modele_id`) REFERENCES `modele`(`id`);
 
 
-ALTER TABLE `Reservation` ADD CONSTRAINT `Reservation_fk0` FOREIGN KEY (`borne_id`) REFERENCES `Bornes`(`id_station`);
+ALTER TABLE `reservation` ADD CONSTRAINT `reservation_fk0` FOREIGN KEY (`borne_id`) REFERENCES `bornes`(`id_station`);
 
-ALTER TABLE `Reservation` ADD CONSTRAINT `Reservation_fk1` FOREIGN KEY (`vehicule_id`) REFERENCES `Vehicule`(`id`);
+ALTER TABLE `reservation` ADD CONSTRAINT `reservation_fk1` FOREIGN KEY (`vehicule_id`) REFERENCES `vehicule`(`id`);
 
-LOAD DATA INFILE 'bornes-irve-borne-irve.csv' INTO TABLE Borne
+LOAD DATA INFILE 'bornes-irve-borne-irve.csv' INTO TABLE `borne`
 FIELDS TERMINATED BY ','
 IGNORE 1 LINES;
