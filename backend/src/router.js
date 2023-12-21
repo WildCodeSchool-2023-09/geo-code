13d0754ce89345a42247e1e382d1d6b242c11f1c
Morgane Debarge <morganedebarge.05@gmail.com>
2023-12-21 16:24:50 +0100

    correction linter

--- backend/src/router.js
+++ backend/src/router.js
@@ -12,8 +12,6 @@ const reservationControllers = require("./controllers/reservationControllers");
 const marqueControllers = require("./controllers/marqueControllers");
 const modeleControllers = require("./controllers/modeleControllers");
 const vehiculeControllers = require("./controllers/vehiculeControllers");
-const enseigneControllers = require("./controllers/enseigneControllers");
-const accessibiliteControllers = require("./controllers/accessibiliteControllers");
 const tarificationControllers = require("./controllers/tarificationControllers");
 const borneControllers = require("./controllers/borneControllers");
 
@@ -64,24 +62,6 @@ router.get("/vehicules/:id", vehiculeControllers.read);
 
 // Route to add a new reservations
 router.post("/vehicules", vehiculeControllers.add);
-
-// Route to get a list of enseignes
-router.get("/enseignes", enseigneControllers.browse);
-
-// Route to get a specific enseignes by ID
-router.get("/enseignes/:id", enseigneControllers.read);
-
-// Route to add a new enseignes
-router.post("/enseignes", enseigneControllers.add);
-
-// Route to get a list of accessibilites
-router.get("/accessibilites", accessibiliteControllers.browse);
-
-// Route to get a specific accessibilites by ID
-router.get("/accessibilites/:id", accessibiliteControllers.read);
-
-// Route to add a new accessibilites
-router.post("/accessibilites", accessibiliteControllers.add);
 
 // Route to get a list of tarifications
 router.get("/tarifications", tarificationControllers.browse);
